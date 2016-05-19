ee380ca0a91df1274c0b4ca293ae859cd6eb01fb
abo <zubo.yu9@gmail.com>
2016-05-19 01:41:33 +0000

    Add one more simpler method to remove duplicates which appears at most twice

--- removedup2.cpp
+++ removedup2.cpp
@@ -26,6 +26,21 @@ int removedup2(int a[], int n)
     return count + 1;
 }
 
+int removedup3(int a[], int n)
+{
+    if (n <= 2)
+        return n;
+
+    int index = 2;
+    for (int i = 2; i < n; i++)
+    {
+        if (a[i] != a[index - 2])
+            a[index++] = a[i];
+    }
+
+    return index;
+}
+
 void dumparray(int a[], int n)
 {
     for (int i = 0; i < n; i++)
